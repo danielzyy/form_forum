83cc945675e0a57c27d7792b6d5e0ce13e75d577
abdullahs26 <abdullahshahid20022002@gmail.com>
2021-01-16 13:05:13 -0500

    initial commit

--- init.php
+++ init.php
@@ -2,12 +2,12 @@
 
 
 try{
-    $db = new PDO('pgsql:host=localhost;port=26257;dbname=defaultdb;sslmode=disable',
-    'admin', null, array(
-      PDO::ATTR_ERRMODE          => PDO::ERRMODE_EXCEPTION,
-      PDO::ATTR_EMULATE_PREPARES => true,
-      PDO::ATTR_PERSISTENT => true
-    ));
+  $db = new PDO('pgsql:host=free-tier.gcp-us-central1.cockroachlabs.cloud;port=26257;dbname=yellow-camel-256.defaultdb;sslmode=require;sslrootcert=C:\Users\sabdu\Desktop',
+  'daniel', '-ElHoins3QXwzCwV', array(
+    PDO::ATTR_ERRMODE          => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_EMULATE_PREPARES => true,
+    PDO::ATTR_PERSISTENT => true
+  ));
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }catch(PDOExeption $e){
     echo "Connection failed : ". $e->getMessage();
