ec3da725446b714700b9d4746332d3d87de80495
abdullahs26 <abdullahshahid20022002@gmail.com>
2021-01-17 02:33:20 -0500

    changed to mysql

--- init.php
+++ init.php
@@ -1,11 +1,12 @@
 <?php
 try{
-    $db = new PDO('pgsql:host=free-tier.gcp-us-central1.cockroachlabs.cloud;port=26257;dbname=yellow-camel-256.defaultdb;sslmode=require;sslrootcert=cc-ca.crt',
-    'user', 'userpassword', array(
-      PDO::ATTR_ERRMODE          => PDO::ERRMODE_EXCEPTION,
-      PDO::ATTR_EMULATE_PREPARES => true,
-      PDO::ATTR_PERSISTENT => true
-    ));
+    // $db = new PDO('pgsql:host=free-tier.gcp-us-central1.cockroachlabs.cloud;port=26257;dbname=yellow-camel-256.defaultdb;sslmode=require;sslrootcert=cc-ca.crt',
+    // 'user', 'userpassword', array(
+    //   PDO::ATTR_ERRMODE          => PDO::ERRMODE_EXCEPTION,
+    //   PDO::ATTR_EMULATE_PREPARES => true,
+    //   PDO::ATTR_PERSISTENT => true
+    // ));
+    $db = new PDO('mysql:dbname=form_forum;host=localhost','root','');
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }catch(PDOExeption $e){
     echo "Connection failed : ". $e->getMessage();
